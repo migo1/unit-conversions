15f29e94b26bea686ed43428be3f88c870c061ed
Robert Migot <robert@verus.africa>
2020-12-30 03:03:01 +0300

    migo package

--- src/Weight.php
+++ src/Weight.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Spatie\UnitConversions;
+namespace Migo\UnitConversions;
 
 class Weight
 {
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Spatie\UnitConversions\Tests;
+namespace Migo\UnitConversions\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Spatie\UnitConversions\Weight;
+use Migo\UnitConversions\Weight;
 
 class WeightTest extends TestCase
 {
